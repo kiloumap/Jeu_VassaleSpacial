6830dc295d33306caa567e1b4094177041cc68fa
Kiloumap <laurent.martinez06@gmail.com>
2017-02-21 15:04:54 +0100

    menu

--- Jeu/Jeu/Program.cs
+++ Jeu/Jeu/Program.cs
@@ -12,17 +12,61 @@ namespace Jeu
     {
         static void Main(string[] args)
         {
-            List<Failure> list = new List<Failure>();
-            foreach(Failure item in BaseFailure.FailureList)
+            // TO DO : convertir le montant int en decimal
+            Console.WriteLine("!! Bonjour !!");
+            ShowMenu();
+            while (true)
             {
-                string res = string.Format(" | Semaine n°{0} | Nom : {1} | Dommage sur : {2} | Dommage : {3} | vie : {4} |", item.numSemaine, item.name, item.typeDamage, item.damage, item.life);
-                Console.WriteLine(res);
+                string operation = Console.ReadLine();
+                switch (operation)
+                {
+                    case "0":
+                        ShowMenu();
+                        break;
+                    case "1":
+                        ShowMenu();
+                        break;
+                    case "2":
+                        ShowMenu();
+                        break;
+                    case "3":
+                        CharacAction();
+                        break;
+                    case "9":
+                        exit();
+                        break;
+                    default:
+                        ShowMenu();
+                        break;
+                }
             }
-            Starship starship = new Starship();
-            string Vaisseau = string.Format(Environment.NewLine + " | Nom du vaisseau : {0} | Vie restante {1} | ", starship.name, starship.life);
+        }
 
-            Console.WriteLine(Vaisseau);
-            Console.Read();
+        private static bool ShowMenu()
+        {
+            string Menu1 = "1 : Voir l'état du vaisseau";
+            string Menu2 = "2 : Voir l'état des membres d'équipages";
+            string Menu3 = "3 : Choisir un membre de l'équipage";
+            string Menu9 = "9 : Abandonner la partie";
+            Console.WriteLine(string.Format("Quelle opération desirez vous faire ? " + Environment.NewLine + "{0}" + Environment.NewLine + "{1}" + Environment.NewLine + "{2}" + Environment.NewLine + "{3}", Menu1, Menu2, Menu3, Menu9));
+            return true;
+        }
+
+        private static bool CharacAction()
+        {
+            string Action0 = "0 : Retour";
+            string Action1 = "1 : Deplacer le personnage";
+            string Action2 = "2 : Lancer un dés";
+            string Action3 = "3 : Stocker les dés restant";
+            string Action4 = "4 : Activer la capacité spécial";
+            Console.WriteLine(string.Format("Quelle opération desirez vous faire ? " + Environment.NewLine + "{0}" + Environment.NewLine + "{1}" + Environment.NewLine + "{2}" + Environment.NewLine + "{3}" + Environment.NewLine + "{4}" , Action0, Action1, Action2, Action3, Action4 ));
+            return true;
+        }
+
+        private static bool exit()
+        {
+            Console.WriteLine(string.Format(Environment.NewLine + "!!! Merci, au revoir !!!"));
+            return false;
         }
     }
 }
